d8f3ac934082472f8c42d46d066521b201b82f94
jieyuz-sf <jieyu.zhang@salesforce.com>
2024-09-11 14:44:10 -0700

    Create attr.py

---
+++ attr.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import json, os
+from tqdm import tqdm
+p = '/export/share/ayan/data/json_jieyu/attr_v2_train.json'
+img_folder = '/export/share/ayan/data/attr'
+a = json.load(open(p, 'r'))
+newa = []
+for c in tqdm(a):
+    try:
+        Image.open(os.path.join(img_folder, c['image'])).convert('RGB')
+    except:
+        print(c)
+        continue
+    newa.append(c)
+print(len(newa))
+json.dump(newa, open(p, 'w'), indent=2)
