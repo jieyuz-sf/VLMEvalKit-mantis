a7a8425ab626e9dd79f2b8a361eb4dcf6bcdf0ee
jieyuz-sf <jieyu.zhang@salesforce.com>
2024-09-13 20:24:30 -0700

    Update attr.py

--- attr.py
+++ attr.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import json, os
 from tqdm import tqdm
-p = '/export/share/ayan/data/json_jieyu/attr_v2_train.json'
+p = '/export/share/ayan/data/json_jieyu/attr_v3_train.json'
 img_folder = '/export/share/ayan/data'
 a = json.load(open(p, 'r'))
 newa = []
